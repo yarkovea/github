a833b3d285bca113e5a2f5e9aae8e54149358491
yarkovea <prpg@mail.ru>
2018-12-20 16:11:25 +0500

    Update lib.php

--- www/lib.php
+++ www/lib.php
@@ -1,11 +1,33 @@
 <?php
-
+    
     echo "<br> <i>файл</i> <b>подключен</b><br>";
-    $i=100;
+    $i=10;
+    
+    // цикл while
+    echo "<hr><br>Цикл <b>while</b><br>";
+    
     echo "цикл $i";
-
     while ($i>0){
         $i--;
         echo '<br>цикл '.$i;
     }
+    echo "<hr><br>Цикл <b>for</b><br>";
+    // цикл for
+    for ($i_f; $i_f<10; $i_f++){
+        echo '<br>'."$i_f";
+    }
+    echo '<br>'."$i_f".'<br>';
+    
+    
+    
+    // цикл foreach
+    echo "<hr><br>Цикл <b>FOREACH</b><br>";
+    $names = array ("Иванов"=>"Андрей","Петров"=>"Борис","Волков"=>"Сергей","Макаров"=>"Федор");
+    //$names["Петров"] = "Борис";
+    //$names["Волков"] = "Сергей";
+    //$names["Макаров"] = "Федор";
+    print_r ($names['Иванов']);
+    foreach ($names as $key => $value) {
+        echo "<b>$value $key</b><br>";
+    }    
 ?>
